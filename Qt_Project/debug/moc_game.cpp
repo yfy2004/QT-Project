0db9f21c7f1ff7700380805be808464e29a87f1f
qxyprzx <115620079+qxyprzx@users.noreply.github.com>
2023-07-08 23:13:25 +0800

    last_update

--- Qt_Project/debug/moc_game.cpp
+++ Qt_Project/debug/moc_game.cpp
@@ -21,8 +21,8 @@ QT_BEGIN_MOC_NAMESPACE
 QT_WARNING_PUSH
 QT_WARNING_DISABLE_DEPRECATED
 struct qt_meta_stringdata_Game_t {
-    QByteArrayData data[6];
-    char stringdata0[71];
+    QByteArrayData data[17];
+    char stringdata0[206];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -36,11 +36,26 @@ QT_MOC_LITERAL(1, 5, 18), // "on_musicon_clicked"
 QT_MOC_LITERAL(2, 24, 0), // ""
 QT_MOC_LITERAL(3, 25, 19), // "on_musicoff_clicked"
 QT_MOC_LITERAL(4, 45, 18), // "on_setting_clicked"
-QT_MOC_LITERAL(5, 64, 6) // "update"
+QT_MOC_LITERAL(5, 64, 6), // "update"
+QT_MOC_LITERAL(6, 71, 13), // "keyPressEvent"
+QT_MOC_LITERAL(7, 85, 10), // "QKeyEvent*"
+QT_MOC_LITERAL(8, 96, 5), // "event"
+QT_MOC_LITERAL(9, 102, 15), // "keyReleaseEvent"
+QT_MOC_LITERAL(10, 118, 10), // "ice_update"
+QT_MOC_LITERAL(11, 129, 11), // "ice_update2"
+QT_MOC_LITERAL(12, 141, 11), // "fire_update"
+QT_MOC_LITERAL(13, 153, 12), // "fire_update2"
+QT_MOC_LITERAL(14, 166, 16), // "animation_update"
+QT_MOC_LITERAL(15, 183, 9), // "game_lose"
+QT_MOC_LITERAL(16, 193, 12) // "game_victory"
 
     },
     "Game\0on_musicon_clicked\0\0on_musicoff_clicked\0"
-    "on_setting_clicked\0update"
+    "on_setting_clicked\0update\0keyPressEvent\0"
+    "QKeyEvent*\0event\0keyReleaseEvent\0"
+    "ice_update\0ice_update2\0fire_update\0"
+    "fire_update2\0animation_update\0game_lose\0"
+    "game_victory"
 };
 #undef QT_MOC_LITERAL
 
@@ -50,7 +65,7 @@ static const uint qt_meta_data_Game[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       4,   14, // methods
+      13,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -58,12 +73,30 @@ static const uint qt_meta_data_Game[] = {
        0,       // signalCount
 
  // slots: name, argc, parameters, tag, flags
-       1,    0,   34,    2, 0x08 /* Private */,
-       3,    0,   35,    2, 0x08 /* Private */,
-       4,    0,   36,    2, 0x08 /* Private */,
-       5,    0,   37,    2, 0x08 /* Private */,
+       1,    0,   79,    2, 0x08 /* Private */,
+       3,    0,   80,    2, 0x08 /* Private */,
+       4,    0,   81,    2, 0x08 /* Private */,
+       5,    0,   82,    2, 0x08 /* Private */,
+       6,    1,   83,    2, 0x08 /* Private */,
+       9,    1,   86,    2, 0x08 /* Private */,
+      10,    0,   89,    2, 0x08 /* Private */,
+      11,    0,   90,    2, 0x08 /* Private */,
+      12,    0,   91,    2, 0x08 /* Private */,
+      13,    0,   92,    2, 0x08 /* Private */,
+      14,    0,   93,    2, 0x08 /* Private */,
+      15,    0,   94,    2, 0x08 /* Private */,
+      16,    0,   95,    2, 0x08 /* Private */,
 
  // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void, 0x80000000 | 7,    8,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void,
@@ -82,10 +115,18 @@ void Game::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void *
         case 1: _t->on_musicoff_clicked(); break;
         case 2: _t->on_setting_clicked(); break;
         case 3: _t->update(); break;
+        case 4: _t->keyPressEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 5: _t->keyReleaseEvent((*reinterpret_cast< QKeyEvent*(*)>(_a[1]))); break;
+        case 6: _t->ice_update(); break;
+        case 7: _t->ice_update2(); break;
+        case 8: _t->fire_update(); break;
+        case 9: _t->fire_update2(); break;
+        case 10: _t->animation_update(); break;
+        case 11: _t->game_lose(); break;
+        case 12: _t->game_victory(); break;
         default: ;
         }
     }
-    Q_UNUSED(_a);
 }
 
 QT_INIT_METAOBJECT const QMetaObject Game::staticMetaObject = {
@@ -113,13 +154,13 @@ int Game::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 4)
+        if (_id < 13)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 4;
+        _id -= 13;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 4)
+        if (_id < 13)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 4;
+        _id -= 13;
     }
     return _id;
 }
